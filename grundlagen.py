64f478aab0cc5fb1d223d6d9c65681ca04f464c6
Kai Schell <kai.schell@signet.de>
2022-10-13 14:27:22 +0200

    Variables and print builtin

---
+++ grundlagen.py
@@ -0,0 +1,24 @@
+# Ich bin ein Kommentar
+
+# Deklaration (Erzeugung) der Variablen foo
+# und Zuweisung des Wertes 7
+foo = 7
+
+# ausgabe des inhalts der variablen foo
+# mit dem python builtin (eingebaute funktion) print()
+print(foo)
+
+# Variablen haben ihren Namen, da sich ihr Inhalt
+# im Laufe des Programms ändern kann
+foo = 2
+print(foo)
+
+# Oft wollen wir die Augabe von Text mit der Ausgabe
+# einer Variablen verbinden
+# Dafür gibt es mehrere Möglichkeiten
+print("Der Inhalt der Variabel foo ist:", foo)
+
+# Stringkonkatenation -> Stringverkettung mit dem + Operator:
+foo = "ball"   # "ball" ist ein String (Zeichenkette)
+bar = "depp"
+print("Fuss" + foo + "spieler" + bar)
